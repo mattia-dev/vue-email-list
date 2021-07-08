64ad602585c4bdfb507e20035762c2a52935ca8c
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-07-08 19:29:12 +0200

    done with bonus

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ new Vue(
     {
         el: "#app",
         data: {
-            email: []
+            emailList: []
         },
         created() {
-            
+            this.getEmailList();
         },
         methods: {
-
-        
+            getEmailList: function() {
+                for (let i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                        this.emailList.push(response.data.response);
+                    }); 
+                }; 
+            },
         }
     }
 )
